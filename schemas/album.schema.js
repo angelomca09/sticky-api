1140978a8f1052295948650af0689f4ad377e689
Angelo Munhoz <angelomca09@gmail.com>
2023-01-12 02:51:14 -0300

    Pseudo Cascade Deleting

--- schemas/album.schema.js
+++ schemas/album.schema.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { User } from "./user.schema.js";
 
 const AlbumSchema = new mongoose.Schema({
   name: String,
@@ -10,6 +11,20 @@ const AlbumSchema = new mongoose.Schema({
       ref: "stickers",
     },
   ],
+});
+
+AlbumSchema.pre("deleteOne", async function (next) {
+  try {
+    const albumId = this.getQuery()["_id"];
+    const users = await User.where({ albums: albumId });
+    for (let user of users) {
+      user.albums = user.albums.filter((s) => !s.equals(albumId));
+      await user.save();
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 const Album = mongoose.model("albums", AlbumSchema);
--- schemas/sticker.schema.js
+++ schemas/sticker.schema.js
@@ -1,9 +1,30 @@
 import mongoose from "mongoose";
+import { User } from "../schemas/user.schema.js";
+import { Album } from "./album.schema.js";
 
 const StickerSchema = new mongoose.Schema({
   name: String,
   number: Number,
   image: String,
+});
+
+StickerSchema.pre("deleteOne", async function (next) {
+  try {
+    const stickerId = this.getQuery()["_id"];
+    const users = await User.where({ stickers: stickerId });
+    for (let user of users) {
+      user.stickers = user.stickers.filter((s) => !s.equals(stickerId));
+      await user.save();
+    }
+    const albums = await Album.where({ stickers: stickerId });
+    for (let album of albums) {
+      album.stickers = album.stickers.filter((s) => !s.equals(stickerId));
+      await album.save();
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 const Sticker = mongoose.model("stickers", StickerSchema);
