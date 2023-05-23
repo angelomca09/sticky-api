f6bcef9c0e0f0401b2f4f67a871f3e4c7069ea19
Angelo Munhoz <angelomca09@gmail.com>
2023-05-23 13:32:05 -0300

    Adding .env

--- db/mongoose.db.ts
+++ db/mongoose.db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 async function connect() {
-  const uri = "mongodb://127.0.0.1:27017/sticky";
+  const uri = process.env.MONGO_URL!;
   return await mongoose.connect(uri)
     .catch((err: Error) => {
       console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
